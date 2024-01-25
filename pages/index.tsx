94bb101683c25a010cb7587a4138b3b99624e9dd
Yvchen <yvchen223@gmail.com>
2024-01-26 00:11:19 +0800

    Reading data from the blockchain

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    try {
+      setAddress(address);
+
+      const key = new web3.PublicKey(address);
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+  
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL)
+      });
+      connection.getAccountInfo(key).then(accountInfo => {
+        setIsExecutable(accountInfo?.executable ?? false);
+      })
+    } catch (error) {
+      setAddress('');
+      setBalance(0);
+      alert(error);
+    }
+    
   }
 
   return (
@@ -23,6 +41,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`IsExecutable: ${isExecutable}`}</p>
       </header>
     </div>
   )
